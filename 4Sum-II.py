79da8d19fe95f0ab847a2fff3a37ee50f46feeab
Sucheta Nandy <56711086+sucheta-nandy@users.noreply.github.com>
2022-02-03 12:20:25 +0530

    Create 4Sum-II.py

---
+++ 4Sum-II.py
@@ -0,0 +1,20 @@
+"""
+Given four integer arrays nums1, nums2, nums3, and nums4 all of length n, return the number of tuples (i, j, k, l) such that:
+
+0 <= i, j, k, l < n
+nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+"""
+
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        n, hm, res = len(nums1), defaultdict(int), 0
+
+        for i in range(n):
+            for j in range(n):
+                hm[nums1[i] + nums2[j]] += 1 
+
+        for k in range(n):
+            for l in range(n):
+                res += hm[0 - (nums3[k] + nums4[l])]
+
+        return res
