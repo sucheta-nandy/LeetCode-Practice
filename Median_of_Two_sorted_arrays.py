99b8172db23bcce5a7b7622c1a0fc503ae412783
Sucheta Nandy <56711086+sucheta-nandy@users.noreply.github.com>
2022-02-08 13:15:36 +0530

    Create Median_of_Two_sorted_arrays.py

---
+++ Median_of_Two_sorted_arrays.py
@@ -0,0 +1,9 @@
+"""
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+"""
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums3 = nums1 + nums2
+        return median(nums3)
