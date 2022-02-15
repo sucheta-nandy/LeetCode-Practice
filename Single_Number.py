b32b461368e9e813cdb29f1442c59529f7c1bec2
Sucheta Nandy <56711086+sucheta-nandy@users.noreply.github.com>
2022-02-15 11:05:41 +0530

    Create Single_Number.py

---
+++ Single_Number.py
@@ -0,0 +1,20 @@
+"""
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+"""
+
+class Solution:
+	def singleNumber(self, nums: List[int]) -> int:
+        
+		frequency={}
+        
+		for i in nums:
+			if i not in frequency:
+				frequency[i]=1
+			else:
+				frequency[i]=frequency[i]+1
+
+		for i in frequency:
+			if frequency[i]==1:
+				return i
